d39c3c8ccf5cfba0e974800324a688a4599ffe91
Ali basit <ali.abdulbasit@tabsusa.com>
2024-08-19 23:28:24 +0500

    documentation responsiveness done

--- src/components/Dacumentations/overview.js
+++ src/components/Dacumentations/overview.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Box, List, ListItem, ListItemText, Typography, Accordion, AccordionSummary, AccordionDetails, Link, Paper } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useTheme, useMediaQuery } from '@mui/material';
+import './Documentation.css'; // Importing the CSS file
 
 const sections = [
   { id: 'overview', label: 'Overview'  },
@@ -55,28 +56,23 @@ const Documentation = () => {
   };
 
   return (
-    <Box display="flex" flexDirection={isMobile ? 'column' : 'row'} bgcolor="#F1F4F5">
+    <Box display="flex" flexDirection={isMobile ? 'column' : 'row'} className="main-container">
       {/* Sidebar */}
-      <Box
-        sx={{ margin: isMobile ? '20px' : '100px 0px 100px 50px' }}
-        width={isMobile ? '100%' : '350px'}
-      >
-        <Box bgcolor="white" sx={{ borderRadius: '20px' ,padding:'15px'}}>
+      <Box className="sidebar">
+        <Box className="sidebar-box">
           <List>
             {sections.map((section) => (
               <ListItem
                 button
                 key={section.id}
                 onClick={() => handleScrollToSection(section.id)}
-                sx={{
-                  color: activeId === section.id ? 'purple' : 'inherit', padding:'15px'
-                }}
+                className={`list-item ${activeId === section.id ? 'active' : ''}`}
               >
                 <ListItemText primary={section.label} />
               </ListItem>
             ))}
             {apiSections.map((section) => (
-              <Accordion sx={{ boxShadow: 'none' }} key={section.id} disableGutters>
+              <Accordion className="accordion" key={section.id} disableGutters>
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                   <Typography>{section.label}</Typography>
                 </AccordionSummary>
@@ -87,10 +83,7 @@ const Documentation = () => {
                         key={`${section.id}-${index}`}
                         button
                         onClick={() => handleScrollToSection(section.id)}
-                        sx={{
-                          pl: 4,
-                          color: activeId === section.id ? 'purple' : 'inherit',
-                        }}
+                        className={`accordion-item ${activeId === section.id ? 'active' : ''}`}
                       >
                         <ListItemText primary={item} />
                       </ListItem>
@@ -102,86 +95,96 @@ const Documentation = () => {
           </List>
         </Box>
       </Box>
-
-      <Box
-        bgcolor="white"
-        sx={{ margin: isMobile ? '20px' : '100px 50px 100px 50px', borderRadius: '20px' }}
-        p={3}
-        width={isMobile ? '100%' : 'calc(100% - 250px)'}
-      >
-        <Box sx={{ padding: '20px', fontFamily: 'Arial, sans-serif', lineHeight: 1.6, color: '#333' }}>
+      <Box className="content-box">
+        <Box className="content">
           <div id="overview">
-            <Typography variant="h4" component="h1" sx={{ fontWeight: 'bold', color: '#333', mb: 2 }}>
+            <Typography
+              variant={isMobile ? 'h5' : 'h4'}
+              component="h1"
+              className="header"
+            >
               Welcome To Fribe Documentation
             </Typography>
-            <Typography>
+            <Typography className='lightText documentation-divider' >
               Welcome to the documentation for our Maps API! This API provides developers with powerful tools to integrate mapping and location-based services into their applications. With our API, you can display maps, generate directions, search for places, and much more.
             </Typography>
           </div>
-          <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold', color: '#333', mt: 4, mb: 2 }}>
+          <Typography
+            variant={isMobile ? 'h6' : 'h5'}
+            component="h2"
+            className="sub-header"
+          >
             API Endpoints And Specifications
           </Typography>
-          <Typography>
+          <Typography className='lightText documentation-divider'>
             For an exhaustive list of API endpoints and specifications, please visit our{' '}
-            <Link href="#" sx={{ color: '#007bff', textDecoration: 'none' }}>
+            <Link href="#" className="custom-link">
               API Docs
             </Link>
             .
           </Typography>
           <div id="authentication">
-            <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold', color: '#333', mt: 4, mb: 2 }}>
+            <Typography
+              variant={isMobile ? 'h6' : 'h5'}
+              component="h2"
+              className="sub-header"
+            >
               Authentication
             </Typography>
           </div>
-          <Typography>
+          <Typography className='lightText'>
             To access the Maps API endpoints, you need to authenticate your requests using OAuth 2.0. Upon successful authentication, you will receive a bearer token that you must include in the header of your API requests.
           </Typography>
 
-          <Typography variant="h6" component="h3" sx={{ fontWeight: 'bold', color: '#333', mt: 3 }}>
+          <Typography variant="h6" component="h3" className="section-subtitle">
             Authentication Method:
           </Typography>
           <List sx={{ listStyleType: 'disc', pl: 2 }}>
-            <ListItem sx={{ display: 'list-item' }}>
+            <ListItem className='lightText' sx={{ display: 'list-item' }}>
               OAuth 2.0 Token Usage:
             </ListItem>
           </List>
 
-          <Typography variant="h6" component="h3" sx={{ fontWeight: 'bold', color: '#333', mt: 3 }}>
+          <Typography variant="h6" component="h3" className="section-subtitle">
             Token Usage:
           </Typography>
-          <Typography>
+          <Typography className='lightText'>
             Include the bearer token received from OAuth 2.0 as an Authorization header in the format:
           </Typography>
-          <Paper elevation={0} sx={{ backgroundColor: '#f8f9fa', padding: '10px', borderRadius: '5px', fontFamily: 'monospace', my: 2 }}>
+          <Paper elevation={0} className="sample-code">
             Authorization: Bearer {'{YOUR_ACCESS_TOKEN}'}
           </Paper>
-          <Typography>
+          <Typography className='lightText documentation-divider'>
             For more details on Authentication, please visit our{' '}
-            <Link href="#" sx={{ color: '#007bff', textDecoration: 'none' }}>
+            <Link href="#" className="custom-link">
               Authentication Guide
             </Link>
             .
           </Typography>
 
-          <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold', color: '#333', mt: 4, mb: 2 }}>
+          <Typography
+            variant={isMobile ? 'h6' : 'h5'}
+            component="h2"
+            className="sub-header"
+          >
             Response Format:
           </Typography>
-          <List sx={{ listStyleType: 'disc', pl: 2 }}>
-            <ListItem sx={{ display: 'list-item' }}>
+          <List  sx={{ listStyleType: 'disc', pl: 2 }}>
+            <ListItem  className='lightTextlist' sx={{ display: 'list-item' }}>
               All responses from the Maps API are returned in JSON format.
             </ListItem>
-            <ListItem sx={{ display: 'list-item' }}>
+            <ListItem className='lightTextlist' sx={{ display: 'list-item' }}>
               Successful responses will include the appropriate data based on the endpoint called.
             </ListItem>
-            <ListItem sx={{ display: 'list-item' }}>
+            <ListItem className='lightTextlist' sx={{ display: 'list-item' }}>
               Error responses will include an error code and message to assist in troubleshooting.
             </ListItem>
           </List>
 
-          <Typography variant="h6" component="h3" sx={{ fontWeight: 'bold', color: '#333', mt: 3 }}>
+          <Typography variant="h6" component="h3" className="section-subtitle ">
             Sample Response:
           </Typography>
-          <Paper elevation={0} sx={{ backgroundColor: '#f8f9fa', padding: '10px', borderRadius: '5px', fontFamily: 'monospace', my: 2 }}>
+          <Paper elevation={0} className="sample-code ">
             {'{'}
             <br />
             &nbsp;&nbsp;"status": "OK",<br />
@@ -192,17 +195,21 @@ const Documentation = () => {
             <br />
             {'}'}
           </Paper>
-          <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold', color: '#333', mt: 4, mb: 2 }}>
+          <Typography
+            variant={isMobile ? 'h6' : 'h5'}
+            component="h2"
+            className="sub-header"
+          >
             Error Handling:
           </Typography>
-          <Typography>
+          <Typography className='lightText'>
             In case of errors, the API will return an appropriate HTTP status code along with a JSON object containing an error message.
           </Typography>
 
-          <Typography variant="h6" component="h3" sx={{ fontWeight: 'bold', color: '#333', mt: 3 }}>
+          <Typography variant="h6" component="h3" className="section-subtitle">
             Sample Error Response:
           </Typography>
-          <Paper elevation={0} sx={{ backgroundColor: '#f8f9fa', padding: '10px', borderRadius: '5px', fontFamily: 'monospace', my: 2 }}>
+          <Paper elevation={0} className="sample-code">
             {'{'}
             <br />
             &nbsp;&nbsp;"status": "error",<br />
@@ -211,17 +218,25 @@ const Documentation = () => {
             {'}'}
           </Paper>
 
-          <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold', color: '#333', mt: 4, mb: 2 }}>
+          <Typography
+            variant={isMobile ? 'h6' : 'h5'}
+            component="h2"
+            className="sub-header"
+          >
             Rate Limiting:
           </Typography>
-          <Typography>
+          <Typography className='lightText documentation-divider' >
             To ensure fair usage and system stability, we enforce rate limiting on API requests. If you exceed the rate limit, you will receive a 429 Too Many Requests response. Please refer to our individual documentations for details on rate limits.
           </Typography>
 
-          <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold', color: '#333', mt: 4, mb: 2 }}>
+          <Typography
+            variant={isMobile ? 'h6' : 'h5'}
+            component="h2"
+            className="sub-header"
+          >
             Conclusion:
           </Typography>
-          <Typography>
+          <Typography className='lightText'>
             Thank you for choosing our Maps! We hope this documentation helps you integrate powerful mapping and location-based features into your applications seamlessly. If you have any questions or need further assistance, feel free to reach out to our support team. Happy mapping!
           </Typography>
         </Box>
